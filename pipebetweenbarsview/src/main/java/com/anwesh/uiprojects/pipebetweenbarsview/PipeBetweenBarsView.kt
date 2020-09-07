7ee9757f3a25a4f3a90a730d3e0d5b736db61bc5
Anwesh43 <anweshthecool0@gmail.com>
2020-09-07 13:33:36 +0530

    adding renderer in view

--- pipebetweenbarsview/src/main/java/com/anwesh/uiprojects/pipebetweenbarsview/PipeBetweenBarsView.kt
+++ pipebetweenbarsview/src/main/java/com/anwesh/uiprojects/pipebetweenbarsview/PipeBetweenBarsView.kt
@@ -66,14 +66,17 @@ fun Canvas.drawPBBNode(i : Int, scale : Float, paint : Paint) {
 
 class PipeBetweenBarsView(ctx : Context) : View(ctx) {
 
-    override fun onDraw(canvas : Canvas) {
+    private val renderer : Renderer = Renderer(this)
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
 
+    override fun onDraw(canvas : Canvas) {
+        renderer.render(canvas, paint)
     }
 
     override fun onTouchEvent(event : MotionEvent) : Boolean {
         when (event.action) {
             MotionEvent.ACTION_DOWN -> {
-
+                renderer.handleTap()
             }
         }
         return true
