72edf986f3f2c2f3115bcd0519e7422a0713c965
Anwesh43 <anweshthecool0@gmail.com>
2020-09-07 13:35:47 +0530

    adding static create method

--- pipebetweenbarsview/src/main/java/com/anwesh/uiprojects/pipebetweenbarsview/PipeBetweenBarsView.kt
+++ pipebetweenbarsview/src/main/java/com/anwesh/uiprojects/pipebetweenbarsview/PipeBetweenBarsView.kt
@@ -215,4 +215,13 @@ class PipeBetweenBarsView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    companion object {
+
+        fun create(activity : Activity) : PipeBetweenBarsView {
+            val view : PipeBetweenBarsView = PipeBetweenBarsView(activity)
+            activity.setContentView(view)
+            return view
+        }
+    }
 }
